08a37ce0c3657e451576ffd8d5793ef734f0acec
FerrariMarc0 <marco1010@live.it>
2023-09-08 17:52:11 +0200

    to-be-continued

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -16,6 +16,24 @@
             double vatIncluded = pc.PricePlusVatCalculator();
 
             Console.WriteLine($"ID prodotto: {productId}\nModello: {name}\nNome completo del modello: {extendedName}\nDescrizione: {description}\nIva: {vat}\nPrezzo base: {basicPrice}\nPrezzo iva compresa: {vatIncluded}");
+
+            Product[] productsArray = new Product[5];
+            productsArray[0] = new Product("Nome1", "Descrizione1", 100, 20);
+            productsArray[1] = new Product("Nome2", "Descrizione2", 200, 21);
+            productsArray[2] = new Product("Nome3", "Descrizione3", 300, 22);
+            productsArray[3] = new Product("Nome4", "Descrizione4", 400, 23);
+            productsArray[4] = new Product("Nome5", "Descrizione5", 500, 24);
+
+            foreach (Product product in productsArray)
+            {
+                if (product != null)
+                {
+                    Console.WriteLine($"Nome: {product.Name}");
+                    Console.WriteLine($"Descrizione: {product.Description}");
+                    Console.WriteLine($"Prezzo Base: {product.Price}");
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
