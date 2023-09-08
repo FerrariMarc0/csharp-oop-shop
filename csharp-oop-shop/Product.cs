4786e46c6ea5336c11aa2eb04bf516eba407f28b
FerrariMarc0 <marco1010@live.it>
2023-09-08 17:15:53 +0200

    without-bonus

---
+++ csharp-oop-shop/Product.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Product
+    {
+        private int productId;
+        private string name;
+        private string description;
+        private double price;
+        private double vat;
+
+        //COSTRUTTORI
+        public Product(string name, string description, double price, double vat)
+        {
+            this.productId = RandomCodeGenerator();
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.vat = vat;
+        }
+
+        //GETTERS & SETTERS
+        public int GetProductId()
+        { return productId; }
+        public string Name
+        { get { return name; } set {  name = value; } }
+        public string Description
+        { get { return description; } set {  description = value; } }
+        public double Price
+        { get { return price; } set { price = value; } }
+        public double Vat { get {  return vat; } set {  vat = value; } }
+
+
+        //METODI
+        public int RandomCodeGenerator()
+        {
+            Random random = new Random();
+            int randomCode = random.Next(10000000, 99999999);
+            return randomCode;
+        }
+
+        public double PricePlusVatCalculator()
+        {
+            double result = price * (vat / 100.0);
+            return result;
+        }
+
+        public string ExtendedProductName()
+        {
+            string extendedName = productId.ToString() + " - " + name;
+            return extendedName;
+        }
+    }
+}
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Creazione nuovo prodotto
+            Product pc = new Product("Dell vostro", " Computer portatile Fattore di forma: Clamshell. Famiglia processore: IntelCore i5 Modello del processore: i5-1135G7. Dimensioni schermo: 396 cm (15.6\") Tipologia HD: Full HD Risoluzione del display: 1920 x 1080 Pixel", 343.00, 21);
+
+            int productId = pc.GetProductId();
+            string name = pc.Name;
+            string extendedName = pc.ExtendedProductName();
+            string description = pc.Description;
+            double basicPrice = pc.Price;
+            double vat = pc.Vat;
+            double vatIncluded = pc.PricePlusVatCalculator();
+
+            Console.WriteLine($"ID prodotto: {productId}\nModello: {name}\nNome completo del modello: {extendedName}\nDescrizione: {description}\nIva: {vat}\nPrezzo base: {basicPrice}\nPrezzo iva compresa: {vatIncluded}");
         }
     }
 }
